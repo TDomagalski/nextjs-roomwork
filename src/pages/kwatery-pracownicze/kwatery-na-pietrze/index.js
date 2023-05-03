d90a6409aea603fad482cb4da12dcf5cfb644d2d
TDomagalski <tomekwebdev@gmail.com>
2023-05-03 19:04:55 +0200

    fix(Kwatery page): fix styles

--- src/pages/kwatery-pracownicze/kwatery-na-pietrze/index.js
+++ src/pages/kwatery-pracownicze/kwatery-na-pietrze/index.js
@@ -1,101 +1,97 @@
 import Link from "next/link";
 import Image from "next/image";
 
-// import ApartRoomIcon from '../../assets/ApartRoomIcon';
-// import ArrowDownIcon from '../../assets/ArrowDownIcon';
+import styles from '../../../styles/pages/Kwatery-version.module.scss';
 
-export default function KwateryParter() {
-	const handleClickScroll = () => {
-		const element = document.getElementById("outlet2");
-
-		if (element) {
-			element.scrollIntoView({ behavior: "smooth" });
-		}
-	};
+export default function KwateryPietro() {
 
 	return (
-		<div className="page_outlet">
-			<h3>Pokój na parterze</h3>
-			<p>
-				Kliknij w pokój, a wyświetlą się niezbędne informacje o pomieszczeniu,
-				wraz ze zdjęciami.
-			</p>
+    <div className={styles.main}>
+      <div className={styles.page_outlet}>
+        <h3>Pokoje na piętrze</h3>
+        <p>
+          Kliknij w pokój, a wyświetlą się niezbędne informacje o pomieszczeniu,
+          wraz ze zdjęciami.
+        </p>
 
-			<div className="offer_nav">
-				<div className="choose_room">
-					<Image
-						src="/arrow-down_icon.svg"
-						alt="Arrow down VV"
-						width={35}
-						height={35}
-						priority
-					/>
-					<h4>Wybierz pokój</h4>
-					<Image
-						src="/arrow-down_icon.svg"
-						alt="Arrow down VV"
-						width={35}
-						height={35}
-						priority
-					/>
-				</div>
-				<Link href="/kwatery/kwatery-parter/pokoj1">
-					<Image
-						src="/rooms-icons/enter_room.svg"
-						alt="Wejdź do pokoju"
-						width={35}
-						height={35}
-						priority
-					/>
-					<span className="span-mobile">1</span>
-					<span className="span-desktop">Pokój 1</span>
-				</Link>
-				<Link href="/kwatery/kwatery-parter/pokoj2">
-					<Image
-						src="/rooms-icons/enter_room.svg"
-						alt="Wejdź do pokoju"
-						width={35}
-						height={35}
-						priority
-					/>
-					<span className="span-mobile">2</span>
-					<span className="span-desktop">Pokój 2</span>
-				</Link>
-				<Link href="/kwatery/kwatery-parter/pokoj3">
-					<Image
-						src="/rooms-icons/enter_room.svg"
-						alt="Wejdź do pokoju"
-						width={35}
-						height={35}
-						priority
-					/>
-					<span className="span-mobile">3</span>
-					<span className="span-desktop">Pokój 3</span>
-				</Link>
-				<Link href="/kwatery/kwatery-parter/pokoj4">
-					<Image
-						src="/rooms-icons/enter_room.svg"
-						alt="Wejdź do pokoju"
-						width={35}
-						height={35}
-						priority
-					/>
-					<span className="span-mobile">4</span>
-					<span className="span-desktop">Pokój 4</span>
-				</Link>
-				<Link href="/kwatery/kwatery-parter/pokoj5">
-					<Image
-						src="/rooms-icons/enter_room.svg"
-						alt="Wejdź do pokoju"
-						width={35}
-						height={35}
-						priority
-					/>
-					<span className="span-mobile">5</span>
-					<span className="span-desktop">Pokój 5</span>
-				</Link>
-			</div>
-			<div id="outlet2">{/* <Outlet /> */}</div>
-		</div>
+        <div className={styles.offer_nav}>
+          <div className={styles.choose_room}>
+            <Image
+              src="/arrow-down_icon.svg"
+              alt="Arrow down VV"
+              width={35}
+              height={35}
+              priority
+            />
+            <h4>Wybierz pokój</h4>
+            <Image
+              src="/arrow-down_icon.svg"
+              alt="Arrow down VV"
+              width={35}
+              height={35}
+              priority
+            />
+          </div>
+          
+          <div className={styles.room_version}>
+            <Link href="/kwatery/kwatery-parter/pokoj1">
+              <Image
+                src="/rooms-icons/enter_room.svg"
+                alt="Wejdź do pokoju"
+                width={35}
+                height={35}
+                priority
+              />
+              <span className={styles.span_mobile}>1</span>
+              <span className={styles.span_desktop}>Pokój 1</span>
+            </Link>
+            <Link href="/kwatery/kwatery-parter/pokoj2">
+              <Image
+                src="/rooms-icons/enter_room.svg"
+                alt="Wejdź do pokoju"
+                width={35}
+                height={35}
+                priority
+              />
+              <span className={styles.span_mobile}>2</span>
+              <span className={styles.span_desktop}>Pokój 2</span>
+            </Link>
+            <Link href="/kwatery/kwatery-parter/pokoj3">
+              <Image
+                src="/rooms-icons/enter_room.svg"
+                alt="Wejdź do pokoju"
+                width={35}
+                height={35}
+                priority
+              />
+              <span className={styles.span_mobile}>3</span>
+              <span className={styles.span_desktop}>Pokój 3</span>
+            </Link>
+            <Link href="/kwatery/kwatery-parter/pokoj4">
+              <Image
+                src="/rooms-icons/enter_room.svg"
+                alt="Wejdź do pokoju"
+                width={35}
+                height={35}
+                priority
+              />
+              <span className={styles.span_mobile}>4</span>
+              <span className={styles.span_desktop}>Pokój 4</span>
+            </Link>
+            <Link href="/kwatery/kwatery-parter/pokoj5">
+              <Image
+                src="/rooms-icons/enter_room.svg"
+                alt="Wejdź do pokoju"
+                width={35}
+                height={35}
+                priority
+              />
+              <span className={styles.span_mobile}>5</span>
+              <span className={styles.span_desktop}>Pokój 5</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
 	);
 }
